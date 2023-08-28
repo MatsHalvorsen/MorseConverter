480c58e5ea9e91a2a75832cbd9239d06ceed08ce
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-08-28 13:13:55 +0200

    added get value from input

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 //model
+const morseCodeArray = [ 
+".-",   // A 
+"-...", // B
+"-.-.", // C
+"-..",  // D
+".",    // E
+".._.", // F
+"--.",  // G
+"....", // H
+"..",   // I
+".---", // J
+"-.-",  // K
+".-..", // L
+"--",   // M
+"-.",   // N
+"---",  // O
+".--.", // P
+"--.-", // Q
+".-.",  // R
+"...",  // S
+"-",    // T
+"..-",  // U
+"...-", // V
+".--",  // W
+"-..-", // X
+"-.--", // Y
+"--..", // Z]
+".-.-", //Æ
+"---. ", //Ø
+".--.- ", //Å
+];
 
-
+//const alphab = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, æ, ø, å]
 
 //view
 updateView();
@@ -9,6 +40,10 @@ function updateView() {
         <div>
         <h1>Morse Converter</h1>
         <h3>Convert text into morse code</h3>
+
+        <input type="text" placeholder="Enter text" onblur="getVal()">
+        <br/>
+        <br/>
         
         </div>
     `
@@ -16,3 +51,18 @@ function updateView() {
 
 
 //controller
+function getVal() {
+    const val = document.querySelector('input').value;
+    //console.log(val)
+    
+    for (let i = 0; i < val.length; i++) {
+        //console.log(val[i])
+        const str = val[i].toString();
+        console.log(str)
+    }
+}
+
+/*function showOutput() {
+
+}
+*/
