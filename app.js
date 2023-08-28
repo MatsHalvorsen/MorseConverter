4a458208da6fc1d1709fb7efc213e036272bdbb8
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-08-28 09:52:31 +0200

    set up MVC

---
+++ app.js
@@ -0,0 +1,18 @@
+//model
+
+
+
+//view
+updateView();
+function updateView() {
+    document.getElementById('app').innerHTML = /*HTML*/ `
+        <div>
+        <h1>Morse Converter</h1>
+        <h3>Convert text into morse code</h3>
+        
+        </div>
+    `
+}
+
+
+//controller
